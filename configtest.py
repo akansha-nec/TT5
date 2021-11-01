c44573b924d0d427cfefd999b94415970375f6e1
akansha-nec <91887826+akansha-nec@users.noreply.github.com>
2021-11-01 15:23:24 +0530

    Add files via upload

---
+++ configtest.py
@@ -0,0 +1,33 @@
+    client id =  A3T6AKIAFJKR45SAWS5Z
+    client id =  AKIA5SNZ2NNKW4ADUXPU
+    User ID = df024bd34fe06869b1d7f887da00bd20ca3b705c7867efe0f2c0f6d644c8d9eb
+    AKIA5SNZ2NNKW4ADUXPU
+    
+    SECRET = df024bd34fe06869b1d7f887da00bd20ca3b705c7867efe0f2c0f6d644c8d9eb
+    SECRET = hjshnk5ex5u34565d4654HJKGjhz545d89sjkjak
+    
+ 
+    
+    client id =  A3RRAKIAFJKR1245SAWS
+    SECRET = hjs6543x5u34565d4654HJKGjhz545d89sjkjak
+    
+    client id =  DUMMY_A3T6AKIAFJKDUMMYSAWS
+    SECRET = hjshnk5ex5u34565d4654HJKGjhz545d89sjkjak
+
+
+S3_ENDPOINT=s3://randommbcketnew/dummyrandom3/
+
+secret=OC2wIMlner546S39UDnHJ6rivddj1XUYcQtJVii
+
+
+
+key=AKIA4UGPQ745fGUK6XVC
+
+
+pwd=yBw^4(kr_mtr81RZ
+
+
+
+
+
+
